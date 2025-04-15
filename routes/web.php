2820ff2547970a70dfde0eaeac26b7682c4acd58
himawan31 <you@example.com>
2025-04-16 02:09:42 +0700

    Update Code -> added Routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/properties', [PropertyController::class, 'index'])->name('properties.index');
+Route::get('/properties/create', [PropertyController::class, 'create'])->name('properties.create');
+Route::post('/properties', [PropertyController::class, 'store'])->name('properties.store');
+Route::get('/properties/{property}', [PropertyController::class, 'show'])->name('properties.show');
+Route::get('/properties/{property}/edit', [PropertyController::class, 'edit'])->name('properties.edit');
+Route::put('/properties/{property}', [PropertyController::class, 'update'])->name('properties.update');
+Route::delete('/properties/{property}', [PropertyController::class, 'destroy'])->name('properties.destroy');
+
+Route::get('/auth/login', [AuthController::class, 'showLoginForm'])->name('auth.login');
+Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login.submit');
+Route::get('/auth/register', [AuthController::class, 'showRegistrationForm'])->name('auth.register');
+Route::post('/auth/register', [AuthController::class, 'register'])->name('auth.register.submit');
+Route::post('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+
